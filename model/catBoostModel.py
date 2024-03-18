6611f681606df933b35a99a3f7b4ca6da2669c58
Archi <129053616+OG-UZOK@users.noreply.github.com>
2024-03-18 13:48:43 +0700

    relize version

--- model/catBoostModel.py
+++
@@ -1,5 +0,0 @@
-from catboost import CatBoostClassifier
-
-class CatBoostModel:
-    def __init__(self, iterations=1000, depth=6, learning_rate=0.1, loss_function='Logloss', logging_level='Silent'):
-        self.model = CatBoostClassifier(iterations=iterations, depth=depth, learning_rate=learning_rate, loss_function=loss_function, logging_level=logging_level)
